6ce220e429405c37b00c291c8b95197fb56aa270
Kevin Gentile <gentile_kevin94@hotmail.com>
2018-02-02 17:38:26 -0500

    begin root handler dev

--- web/main.go
+++ web/main.go
@@ -1,8 +1,23 @@
 package main
 
-import "log"
+import (
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
 
 func main() {
 	log.Println("fortnite-uptime-tracker web start")
 
+	r := mux.NewRouter()
+
+	r.HandleFunc("/", rootHandler)
+
+	log.Fatal(http.ListenAndServe(":8080", r))
+
+}
+
+func rootHandler(w http.ResponseWriter, r *http.Request) {
+	log.Println("serving root")
 }
