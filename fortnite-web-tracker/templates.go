51234436f17a31c41a413ca786c1dd503934c617
Kevin Gentile <gentile_kevin94@hotmail.com>
2018-02-02 18:28:43 -0500

    update error handling

--- fortnite-web-tracker/templates.go
+++ fortnite-web-tracker/templates.go
@@ -14,23 +14,21 @@ var templates = ParseTemplates()
 //ParseTemplates walks the template directory, parses all html templates, and returns a template pointer.
 func ParseTemplates() *template.Template {
 	tmp := template.New("")
-	err := filepath.Walk("./templates", func(path string, info os.FileInfo, err error) error {
-		if strings.Contains(path, "template.html") {
-			_, err = tmp.ParseFiles(path)
-			if err != nil {
+	if err := filepath.Walk("./templates", func(path string, info os.FileInfo, err error) error {
+		if strings.Contains(path, "tmpl.html") {
+			if _, err := tmp.ParseFiles(path); err != nil {
 				log.Println(err)
 			}
 		}
 		return nil
-	})
-	if err != nil {
+	}); err != nil {
 		panic(err)
 	}
 	return tmp
 }
 
 func renderTemplate(w http.ResponseWriter, tmpl string, data interface{}) {
-	tmpl = tmpl + ".template.html"                  //add html tag
+	tmpl = tmpl + ".tmpl.html"                      //add html tag
 	err := templates.ExecuteTemplate(w, tmpl, data) //attempt to render the template
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError) //Template not found/ not rendered
