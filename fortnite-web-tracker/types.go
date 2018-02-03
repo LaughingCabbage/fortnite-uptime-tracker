9508eba0ac766b81c43c00caadb1d40736c732ab
Kevin Gentile <gentile_kevin94@hotmail.com>
2018-02-02 19:00:54 -0500

    add data map. fix json name

--- fortnite-web-tracker/types.go
+++ fortnite-web-tracker/types.go
@@ -1,10 +1,13 @@
 package main
 
+//Data is used to pass various child structures from a handler for templating
+type Data map[string]interface{}
+
 type ServerStatus struct {
 	ServiceInstanceID  string       `json:"serviceInstanceId"`
 	Status             string       `json:"status"`
 	Message            string       `json:"message"`
-	MaintenenceURL     string       `json:"maintenenceUrl"`
+	MaintenenceURL     string       `json:"maintenanceUri"`
 	OverrideCatalogIDs []string     `json:"overrideCatalogIds"`
 	AllowedActions     []string     `json:"allowedActions"`
 	Banned             bool         `json:"banned"`
